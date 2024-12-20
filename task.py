1dc5c9ab5199c46c693fa425f60767b93b02e8d8
kuvinig <kuvi_work@outlook.com>
2024-12-20 16:02:34 +0700

    Функция ручного ввода массива

--- task.py
+++ task.py
@@ -4,7 +4,22 @@ import random
 def generate_array(size, lower_bound=1, upper_bound=100):
     return [random.randint(lower_bound, upper_bound) for _ in range(size)]
 
-# Тестирование функции
+# Ручной ввод массива
+def input_array():
+    size = int(input("Введите размер массива: "))
+    array = []
+    for i in range(size):
+        number = int(input(f"Введите элемент массива {i + 1}: "))
+        array.append(number)
+    return array
+
+# Тестирование функций
 if __name__ == "__main__":
-    arr = generate_array(10)
-    print("Сгенерированный массив:", arr)
+    # Выбор источника данных
+    choice = input("Выберите источник данных: 1 - Генерация, 2 - Ввод вручную: ")
+    if choice == '1':
+        arr = generate_array(10)
+        print("Сгенерированный массив:", arr)
+    elif choice == '2':
+        arr = input_array()
+        print("Введенный массив:", arr)
